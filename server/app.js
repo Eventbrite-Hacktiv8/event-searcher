6781bbc75d1df7702914bac78e718bd989a2bfac
YusufBaraja <baraja.yusuf@gmail.com>
2019-09-05 19:04:50 +0700

    initial project

--- server/app.js
+++ server/app.js
@@ -1,4 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
 
+require('dotenv').config()
+const app = require('express')();
+const PORT = process.env.PORT || 3000
+const routes = require('./routes');
+const body = require('body-parser');
+const cors = require('cors')
+
+app.use(body.urlencoded({ extended: false }))
+app.use(body.json())
+app.use('/', routes);
+app.use(cors())
+mongoose.connect('mongodb://localhost:27017/hacktivGit', {useNewUrlParser: true})
+.then(data => {
+    console.log('success')
+}).catch(err => {
+    console.log('error')
+})
+
+app.listen(PORT, function () {
+    console.log(`listen to port ${PORT}`)
+})
