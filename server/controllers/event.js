d72023fa834afe937bfd9e00a0794392ad25e25f
igun369 <guntoro.gyk@gmail.com>
2019-09-05 22:53:58 +0700

    created endpoint to get events and category

--- server/controllers/event.js
+++ server/controllers/event.js
@@ -1,9 +1,45 @@
+const axiosEvent = require('../config/axiosEvent');
 
 class EventController {
     static getAllEvents(req, res, next) {
-        res.status(200).json({
-            message: 'connected'
+        axiosEvent.get('/search/?location.address=Jakarta')
+            .then(events => {
+                // console.log(events.data)
+                res.status(200).json(events.data);
+            })
+            .catch(next);
+    }
+
+    static filter(req, res, next) {
+        let fields = Object.keys(req.query);
+        let values = Object.values(req.query);
+
+        console.log(fields)
+        console.log(values)
+        let searchUri = '';
+
+        fields.forEach((field, i) => {
+            searchUri += `${field}=${values[i]}&`;
         })
+
+        console.log(searchUri)
+        axiosEvent.get(`/search/?${searchUri}`)
+            .then(events => {
+                res.status(200).json(events.data);
+            })
+            .catch(next);
+    }
+
+    static categories(req, res, next) {
+        axiosEvent.get('/categories')
+            .then(categories => {
+                res.status(200).json(categories.data);
+            })
+            .catch(next)
+    }
+
+    static bookmark(req, res, next) {
+        
     }
 }
 
--- server/routes/event.js
+++ server/routes/event.js
@@ -3,4 +3,8 @@ const EventController = require('../controllers/event');
 
 router.get('/', EventController.getAllEvents);
 
+router.get('/search/', EventController.filter);
+
+router.get('/categories', EventController.categories)
+
 module.exports = router;
