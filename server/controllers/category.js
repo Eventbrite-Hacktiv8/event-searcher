fb05cad7f5592d434e7949752a2df7816ee5e537
igun369 <guntoro.gyk@gmail.com>
2019-09-06 11:07:47 +0700

    added search by city

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -4,7 +4,7 @@ class CategoryController {
     static getCategories(req, res, next) {
         axiosEventbrite.get('/categories')
             .then(({data}) => {
-                console.log(data, 'data categories');
+                // console.log(data, 'data categories');
                 res.status(200).json(data.categories);
             })
             .catch(next)
