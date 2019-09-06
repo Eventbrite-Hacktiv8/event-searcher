073aeb3a9931a2439341e1ccb0fbc48d23f2dbb8
igun369 <guntoro.gyk@gmail.com>
2019-09-06 09:12:33 +0700

    list events and save event done

--- client/js/script.js
+++ client/js/script.js
@@ -19,18 +19,21 @@ function getEvents() {
         })
 }
 
-function createCard(event) {
+function createCard(event, i) {
     let date = formatDate(event.start);
     let card = `
     <div class="col-sm-3 d-flex">
         <div class="card" style="width: 22rem;">
-            <img src="${event.logo.url}" alt="">
-            <div class="card-body">
-                <p class="mb-0" style="font-size: 0.8rem">${date}</p>
-                <strong class="" style="font-size: 1.2rem">${event.name}</strong>
-                <p style="font-size: 0.9rem">${event.venue.name}</p>
-                <a href="#" class="btn btn-primary">Save</a>
-            </div>
+                <span class="eventId" hidden>${event.id}</span>
+                <span class="eventAddress" hidden>${event.venue.address}</span>
+                <img src="${event.logo.url}" alt="" id="imgUrl" class="imgUrl">
+                <div class="card-body">
+                    <p class="mb-0 date" style="font-size: 0.8rem" id="date">${date}</p>
+                    <strong class="eventName" id="eventName" style="font-size: 1.2rem">${event.name}</strong>
+                    <p class="venue" id="venue" style="font-size: 0.9rem">${event.venue.name}</p>
+                    <button class="btn btn-primary" onclick="saveEvent(this)" data-index=${i}>Save</button>
+                </div>
+           
         </div>
     </div>
     `
@@ -38,8 +41,8 @@ function createCard(event) {
 }
 
 function showCard(data) {
-    data.forEach(event => {
-        let card = createCard(event);
+    data.forEach((event, i) => {
+        let card = createCard(event, i);
         $('#eventList').append(card)
     });
 }
@@ -58,4 +61,44 @@ function formatDate(strDate) {
     }
 
     return `${dd}-${mm}-${year}`;
+}
+
+
+function saveEvent(event) {
+    let index = event.getAttribute('data-index');
+    console.log(event)
+    console.log(index)
+
+    // console.log($('.eventName')[Number(index)])
+    // console.log($('.eventName')[Number(index)].textContent)
+    let eventId = $('.eventId')[Number(index)].textContent;;
+    let name = $('.eventName')[Number(index)].textContent;
+    let venue = $('.venue')[Number(index)].textContent;
+
+    console.log(eventId)
+    console.log(name)
+    console.log(venue)
+    axios({
+        url: `${baseURL}/events`,
+        method: 'POST',
+        data: {
+            eventId,
+            name,
+            venue
+        }
+    })
+        .then((response) => {
+            console.log(response)
+            alert(response.data.message)
+        })
+        .catch(err => {
+            console.log(err)
+            if (err.request) {
+                console.log(err.request)
+                alert(err.request.responseText)
+            } else {
+                console.log(err);
+            }
+            // alert(err.message)
+        })
 }
